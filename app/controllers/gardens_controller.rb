23babec4d669de67fe4e8ae5d69205d6dbbe442a
Carina Sweet <carina.sweet06@gmail.com>
2021-08-06 13:55:19 -0600

    Refactor user story 3

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,8 +1,5 @@
 class GardensController < ApplicationController
   def show
     @garden = Garden.find(params[:id])
-    require "pry";binding.pry
-    @garden_plants = @garden.plots.plants.garden_plants_below_100_day_harvest
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :plots, only: [:index]
 
-  delete '/plots/:plot_id/plants/:plant_id', to: 'plot_plants#destory'
+  delete '/plots/:plot_id/plants/:plant_id', to: 'plot_plants#destroy'
 
   resources :gardens, only: [:show]
 end
